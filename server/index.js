1dda4231f96ca3cdd2614ed57328caebe182aced
Hung Dinh <hung.dq1997hn@gmail.com>
2023-06-09 13:48:28 -0700

    second-commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import 'dotenv/config';
 
 import postRoutes from './routers/post.js';
 import useRoutes from './routers/user.js';
@@ -15,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', useRoutes);
-const CONNECTION_URL = 'mongodb+srv://hung:!23456@cluster0.yptzo9p.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.MONGODB_LINK;
 const PORT = process.env.PORT || 5000;
 app.get('/', (req,res) =>{
     res.send('APP IS RUNNING');
